8e3a0190cf95fa0203f0b28f4725b8c760e64e1e
you@example.com <you@example.com>
2025-08-14 05:59:39 +0000

    Update deployment

--- _site/assets/js/just-the-docs.js
+++ _site/assets/js/just-the-docs.js
@@ -77,7 +77,7 @@ function disableHeadStyleSheets() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', '/maths/assets/js/search-data.json', true);
+  request.open('GET', '/documentations/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -465,7 +465,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', '/maths/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', '/documentations/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Note: pathname can have a trailing slash on a local jekyll server
